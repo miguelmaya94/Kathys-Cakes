7439a547f1895f3667f689369c5c2f62eece09e5
Miguel Maya <miguel.maya88@yahoo.com>
2021-11-04 18:53:38 -0700

    bugs and animations

--- register.php
+++ register.php
@@ -80,7 +80,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
-                header("location: order.php");
+                header("location: other.php");
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
